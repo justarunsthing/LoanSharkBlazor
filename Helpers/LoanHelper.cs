da237e75aafb1f91a8b27347e6f215c53d0a0075
justarunsthing <justarunsthing@outlook.com>
2025-11-04 08:33:43 +0000

    Added LoanHelper class

---
+++ Helpers/LoanHelper.cs
@@ -0,0 +1,26 @@
+﻿namespace LoanSharkBlazor.Helpers
+{
+    public static class LoanHelper
+    {
+        /// <summary>
+        /// Calculates a payment for a simple interest loan.
+        /// </summary>
+        /// <param name="amount">Loan Amount</param>
+        /// <param name="rate">Anualized rate as a double</param>
+        /// <param name="term">Term in years</param>
+        /// <returns>A monthly payment as a double</returns>
+        public static double CalculatePayment(double amount, double rate, double term)
+        {
+            var monthlyRate = CalculateMonthlyRate(rate);
+            var months = term * 12;
+            var payment = (amount * monthlyRate) / (1 - Math.Pow(1 + monthlyRate, -months));
+
+            return payment;
+        }
+
+        private static double CalculateMonthlyRate(double rate)
+        {
+            return rate / 1200;
+        }
+    }
+}
