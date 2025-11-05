ea1d56e85f0222ad1d9bb04bbafd047b4e049d71
justarunsthing <justarunsthing@outlook.com>
2025-11-05 08:33:31 +0000

    Added GetPayments helper method

--- Helpers/LoanHelper.cs
+++ Helpers/LoanHelper.cs
@@ -1,7 +1,49 @@
-﻿namespace LoanSharkBlazor.Helpers
+﻿using LoanSharkBlazor.Models;
+
+namespace LoanSharkBlazor.Helpers
 {
     public static class LoanHelper
     {
+        /// <summary>
+        /// Caculate the monthly payment schedule for a loan
+        /// </summary>
+        /// <param name="loan"></param>
+        /// <returns>Returns a loan object</returns>
+        public static Loan GetPayments(Loan loan)
+        {
+            double balance = loan.PurchaseAmount;
+            double totalInterest = 0.0;
+            double monthlyPrincipal = 0.0;
+            double monthlyInterest = 0.0;
+            double monthlyRate = CalculateMonthlyRate(loan.Rate);
+            var loanMonths = loan.Term * 12;
+
+            loan.Payment = CalculatePayment(loan.PurchaseAmount, loan.Rate, loan.Term);
+            loan.AmortizationSchedule.Clear();
+
+            for (int month = 1; month <= loanMonths; month++)
+            {
+                monthlyInterest = CalculateMonthlyInterest(balance, monthlyRate);
+                totalInterest += monthlyInterest;
+                monthlyPrincipal = loan.Payment - monthlyInterest;
+                balance -= monthlyPrincipal;
+
+                var loanPayment = new LoanPayment
+                {
+                    Month = month,
+                    Payment = loan.Payment,
+                    MonthlyPrincipal = monthlyPrincipal,
+                    MonthlyInterest = monthlyInterest,
+                    TotalInterest = totalInterest,
+                    Balance = balance < 0 ? 0 : balance
+                };
+
+                loan.AmortizationSchedule.Add(loanPayment);
+            }
+
+            return loan;
+        }
+
         /// <summary>
         /// Calculates a payment for a simple interest loan.
         /// </summary>
@@ -21,6 +63,11 @@
         private static double CalculateMonthlyRate(double rate)
         {
             return rate / 1200;
+        }
+
+        private static double CalculateMonthlyInterest(double balance, double monthlyRate)
+        {
+            return balance * monthlyRate;
         }
     }
 }
