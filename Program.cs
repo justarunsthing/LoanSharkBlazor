4e798fdba695b19ae2214b70fe36d066f44fa4ba
arunpun <83875939+arunpun@users.noreply.github.com>
2025-12-22 08:47:06 +0000

    Set culture

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using LoanSharkBlazor;
+using System.Globalization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -7,5 +8,9 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+var culture = new CultureInfo("en-GB");
+CultureInfo.DefaultThreadCurrentCulture = culture;
+CultureInfo.DefaultThreadCurrentUICulture = culture;
 
 await builder.Build().RunAsync();
